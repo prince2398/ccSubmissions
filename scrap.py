8f264aede77342f34966ebcc0caa562123fbd1da
prince2398 <princepiyush2398@gmail.com>
2017-10-01 18:21:22 +0530

    can extract all the submission urls

--- scrap.py
+++ scrap.py
@@ -1,26 +1,55 @@
 import requests
 from bs4 import BeautifulSoup
+import re
+from pprint import pprint
 
-uname = 'pp2398'
-url = 'https://www.codechef.com/submissions?sort_by=All&sorting_order=asc&language=All&status=All&year=2017&handle='+uname+'&pcode=&ccode=&Submit=GO'
+uName = 'nikhilksingh97'
 
-resp = requests.get(url)
+def getURL(uName,pg):
+    subURLs=[]
 
-soup = BeautifulSoup(resp.content,'html5lib')
+    for Pg in range(0,pg):
 
-table = soup.findAll('div',attrs = {'class':'tablebox'})
+        url = 'https://www.codechef.com/submissions?page='+str(Pg)+'&sort_by=All&sorting_order=asc&language=All&status=All&year=2017&handle='+uName+'&pcode=&ccode=&Submit=GO'
 
-tbody = table[0].table.tbody
+        resp = requests.get(url)
 
-submissions = tbody.findAll('tr')
+        soup = BeautifulSoup(resp.content,'html5lib')
 
-subnos=[]
+        table = soup.findAll('div',attrs = {'class':'tablebox'})
 
-for sub in submissions:
-    no = sub.td.text
-    subnos.append(no)
-    
+        tbody = table[0].table.tbody
 
-for sNo in subnos:
-    url = 'https://www.codechef.com/viewsolution/'+sNo
-    print(url+'\n')
+        submissions = tbody.findAll('tr')
+
+        subnos=[]
+
+        for sub in submissions:
+            no = sub.td.text
+            subnos.append(no)
+
+
+        for sNo in subnos:
+            sNo = 'https://www.codechef.com/viewsolution/'+sNo
+            subURLs.append(sNo)
+
+    return subURLs
+
+def getPg(uName):
+    url= 'https://www.codechef.com/submissions?sort_by=All&sorting_order=asc&language=All&status=All&year=2017&handle='+uName+'&pcode=&ccode=&Submit=GO'
+    page = requests.get(url)
+
+    soup = BeautifulSoup(page.content,'html5lib')
+
+    soup = soup.findAll('div',attrs = {'class':'pageinfo'})
+    soup = soup[0].text
+    soup = re.findall('\d+',soup)
+    Pg = soup[1]
+    return Pg
+
+pg=int(getPg(uName))
+URLs = getURL(uName,pg)
+subCount = len(URLs)
+print(pg)
+print("total no. of submissions = "+str(subCount))
+pprint(URLs)
