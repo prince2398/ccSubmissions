42866ab84c7e431e209366e74db507d749a42c6a
prince2398 <princepiyush2398@gmail.com>
2017-10-02 02:50:25 +0530

    Completed .. just change the username and get the submissions in file

--- scrap.py
+++ scrap.py
@@ -2,43 +2,91 @@ import requests
 from bs4 import BeautifulSoup
 import re
 from pprint import pprint
+import os
+cnt=0
 
-uName = 'singh_prishita'
+#to return extension of file
+def calcExt(lang) :
+    return {
+        'C' : 'cpp',
+        'PYTH' : 'py',
+        'JAVA' : 'java',
+        'C99' : 'cpp',
+        'ADA' : 'ada',
+        'PYPY' : 'py'
+    }[lang]
+#To write in  a file
+def writeCode(filename,code):
+    global cnt
+    codeFile = open(filename,'a')
+
+    for line in code:
+        codeFile.write(line.text)
+        codeFile.write('\n')
+
+    codeFile.close()
+    cnt+=1
+#main function to write code in a file by sub no.
+def subNoToFile(subNo):
+    url = 'https://www.codechef.com/viewsolution/'+subNo+'/index.html'
+    #to get html content
+    resp = requests.get(url)
+    soup = BeautifulSoup(resp.content,'html5lib')
+    soup = soup.findAll('div',attrs = {'id': 'solutiondiv'})
+
+    if len(soup)!=0:
+        soup = soup[0]
+
+        #to assign filename
+        head = soup.title.text
+        title = re.findall('\w+',head)
+        ext = calcExt(title[3])
+        filename = subNo+'.'+ext
+        try:
+            os.makedirs('submissions')
+        except OSError:
+            pass
+        path = 'submissions/'+filename
+
+        #to get submitted code in list line by line
+
+        code = soup.pre.ol
+        code = code.findAll('li')
+
+        writeCode(path,code)
 
 
-def getURL(uName,pg,yr):
-    subURLs=[]
+#to get list of submission ids of a searched year and a particular page
+def getSubNo(uName,pg,yr):
+    subNos=[]
 
     for Pg in range(0,pg):
 
         url = 'https://www.codechef.com/submissions?page='+str(Pg)+'&sort_by=All&sorting_order=asc&language=All&status=All&year='+str(yr)+'&handle='+uName+'&pcode=&ccode=&Submit=GO'
 
         resp = requests.get(url)
-
         soup = BeautifulSoup(resp.content,'html5lib')
-
         table = soup.findAll('div',attrs = {'class':'tablebox'})
 
         if len(table)!=0:
-
             tbody = table[0].table.tbody
-
             submissions = tbody.findAll('tr')
-
             for sub in submissions:
                 no = sub.td.text
                 if no!='No Recent Activity':
-                    subURLs.append('https://www.codechef.com/viewsolution/'+no)
+                    subNos.append(no)
 
-    return subURLs
+    return subNos
 
+
+#to get total no. of pages in a search by username and year
 def getPg(uName,yr):
     url= 'https://www.codechef.com/submissions?sort_by=All&sorting_order=asc&language=All&status=All&year='+str(yr)+'&handle='+uName+'&pcode=&ccode=&Submit=GO'
+
     page = requests.get(url)
-
     soup = BeautifulSoup(page.content,'html5lib')
-
     soup = soup.findAll('div',attrs = {'class':'pageinfo'})
+
     if len(soup)!=0 :
         soup = soup[0].text
         soup = re.findall('\d+',soup)
@@ -47,14 +95,25 @@ def getPg(uName,yr):
     else:
         return 1;
 
-URLs = []
 
-for yr in range(2017,2009,-1):
-    pg=int(getPg(uName,yr))
-    URLs = URLs + getURL(uName,pg,yr)
-subCount = len(URLs)
-pprint(URLs)
-print("total no. of submissions = "+str(subCount))
+#to get total list of all submission ids of a particular user
+def subList(username):
+    subs = []
+
+    for yr in range(2017,2009,-1):
+        pg=int(getPg(username,yr))
+        subs = subs + getSubNo(username,pg,yr)
+
+    return subs
 
 
-#Got URLS of Every submission by a user above(from all previous years)
+
+#main program from username to files
+uName = 'pp2398'
+
+Subs = subList(uName)
+for Sub in Subs:
+    subNoToFile(Sub)
+no = len(Subs)
+msg = str(cnt)+'/'+str(no)+" no. of submmissions are extracted."
+print(msg)
